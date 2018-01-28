89ad632e1ae008d1367be65bdbe0bd35e96fa80b
Mao <lmao@ak120d-15.wpi.edu>
2018-01-28 16:25:34 -0500

    lalala

--- src/Lab2_preLab.m
+++ src/Lab2_preLab.m
@@ -19,9 +19,13 @@ import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.lang.*;
 
-L1 = 135
-L2 = 175
-L3 = 169.28
+L1 = 135;
+L2 = 175;
+L3 = 169.28;
+theta = 10;
+alpha = 5;
+a = 2;
+d= 2;
 % Create a PacketProcessor object to send data to the nucleo firmware
 pp = PacketProcessor(7); 
 SERV_ID = 44;            % we will be talking to server ID 37 on
@@ -55,20 +59,12 @@ for k = 0:1
    
     pause(1) %timeit(returnPacket) 
 end
+
+
+
 P1 = Transform(returnPacket(1),0,L1,0);
 P2 = P1*Transform(returnPacket(3),0,L2,0);
 P3 = P2*Transform(returnPacket(7),0,L3,0);
-Pt1 = P1(1:3,4);
-Pt2 = P2(1:3,4);
-Pt3 = P3(1:3,4);
-
-function [T] = Transform (theta, alpha, a, d) 
-T = [ cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta); 
-      sin(theta) -cos(theta)*cos(alpha) -cos(theta)*sin(alpha) a*sin(theta);
-      0 sin(alpha) cos(alpha) d;
-      0 0 0 1];
-end
-
 
 
 % Clear up memory upon termination
@@ -76,6 +72,19 @@ pp.shutdown()
 clear java;
 
 toc
+
+plot3([0,P1(1,4),P2(1,4),P3(1,4)],[0,P1(2,4),P2(2,4),P3(2,4)],[0,P(3,4),P2(3,4),P3(3,4)]);
+
+function [T] = Transform (theta, alpha, a, d) 
+T = [cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta); 
+      sin(theta) -cos(theta)*cos(alpha) -cos(theta)*sin(alpha) a*sin(theta);
+      0 sin(alpha) cos(alpha) d;
+      0 0 0 1];
+end
+
+function [r] = rev (t)
+r= t/4096*360;
+end
 
 
 
