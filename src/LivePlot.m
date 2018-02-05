16457816801802db8266db1e9f89a3ec44da4376
Mao <lmao@ak120d-11.wpi.edu>
2018-02-05 17:20:57 -0500

    part5

--- src/LivePlot.m
+++ src/LivePlot.m
@@ -1,4 +1,4 @@
-function [PLOTTT ] = LivePlot( )
+function [PLOTTT,Enddd] = LivePlot( )
     PLOTTT = plot3(0,0,0,'-o');
     grid('on')
     xlim([-300 400])
@@ -7,8 +7,14 @@ function [PLOTTT ] = LivePlot( )
     xlabel('x')
     ylabel('y')
     zlabel('z')
+    title('Robot Live Plot')
     set(PLOTTT,'XDataSource','ACTUALX');
     set(PLOTTT,'YDataSource','ACTUALY');
     set(PLOTTT,'ZDataSource','ACTUALZ');
+    hold on;
+    Enddd = plot3(0,0,0,'-');
+    set(Enddd,'XDataSource','OutputTIP(:,1)');
+    set(Enddd,'YDataSource','OutputTIP(:,2)');
+    set(Enddd,'ZDataSource','OutputTIP(:,3)');
 end
 
--- src/test.m
+++ src/test.m
@@ -36,20 +36,46 @@ DEBUG   = true;          % enables/disables debug prints
 packet = zeros(15, 1, 'single');
 
 timeline = [];
-PLOTTT = LivePlot();
+
+
 %EndPlottt = EndPlot();
 OutputTIP = zeros(0, 3, 'single');%3*0 vector
+OutputAngle = zeros(0, 3, 'single');%3*0 vector
+[PLOTTT,Enddd] = LivePlot();
 
-    qX = [175, 146.1462,154.7008,175];
-    qY = [0,  -164.3877,  119.3989,0];
-    qZ = [-34.28,  -20.7257,  300,-34.28];   
+   
+    TrajX = LinearTraj(1,101);
+    %qX = [175,      146.1462,   154.7008]; question#3
+    %qY = [0,        -164.3877,  119.3989];
+    %qZ = [-34.28,   -20.7257,   300];  
+
+    %qX = [175,      146.1462,   154.7008]; question #4
+    %qY = [210,        -164.3877,  119.3989];
+    %qZ = [110,   -20.7257,   300];
     
-
+    TrajX1 = LinearTraj(175,146.1462);
+    TrajX2 = LinearTraj(146.1462,154.7008);
+    TrajX3 = LinearTraj(154.7008,175);
+    TrajY1 = LinearTraj(210,-164.3);
+    TrajY2 = LinearTraj(-164.3,119.39);
+    TrajY3 = LinearTraj(119.39,210);
+    TrajZ1 = LinearTraj(110,-20.72);
+    TrajZ2 = LinearTraj(-20.72,300);
+    TrajZ3 = LinearTraj(300,110);
+    qX = [175,      TrajX1,   TrajX2,  TrajX3]; 
+    qY = [210,      TrajY1,   TrajY2,  TrajY3];
+    qZ = [110,      TrajZ1,   TrajZ2,  TrajZ3];
+    disp(qX);
+    disp(qY);
+    disp(qZ);
 % Iterate through a sine wave for joint values
 k = 1;
 tic
-while  k<5
-
+time = 0;
+while time<40
+    if k > 31
+        k = 1;
+    end
     [A,B,C] = ikin3001(qX(k),qY(k),qZ(k),L1,L2,L3);
     packet = GotoPosition(packet,A,B,C);
     % Send packet to the server and get the response
@@ -59,22 +85,51 @@ while  k<5
     
     [ACTUALX,ACTUALY,ACTUALZ,TIP] = Forkin(returnPacket,L1,L2,L3);
     OutputTIP = vertcat(OutputTIP,TIP);%concatenate returnpacket onto vactor to form a matrix
+    OutputAngle = vertcat(OutputAngle,[A,B,C]);%concatenate returnpacket onto vactor to form a matrix
     timeline = [timeline,time];
+    
     refreshdata(PLOTTT)
-    pause(0.1) %timeit(returnPacket)
+    refreshdata(Enddd)
+    pause(0.03) %timeit(returnPacket)
 
     toler = 30;
     if abs(TIP(1)-qX(k)) <= toler &&  abs(TIP(2)-qY(k)) <= toler &&  abs(TIP(3)-qZ(k)) <= toler
       k = k+1;
     end
-    disp(time)
-    disp(TIP)
-    disp('true')
-  
+
 end
-disp(timeline)
-disp(OutputTIP)
-%plot(timeline,OutputTIP);
+
+figure(2)
+plot(timeline,OutputTIP);
+xlabel('Time(s)')
+ylabel('Position(mm)')
+legend('X','Y','Z')
+title('Robot Tip Position Vs Time')
+
+figure(3)
+plot(timeline,OutputAngle);
+xlabel('Time(s)')
+ylabel('Angles(tic)')
+legend('Axis1','Axis2','Axis3')
+title('Robot Joint Angle Vs Time')
+
+OutputTIPV = diff(OutputTIP);
+figure(4)
+disp(timeline(:,2:end));
+plot(timeline(:,2:end),OutputTIPV);
+xlabel('Time(s)')
+ylabel('PositionVelocity(mm/s)')
+legend('X','Y','Z')
+title('Robot Tip Velocity Vs Time')
+
+OutputTIPA = diff(OutputTIPV);
+figure(5)
+plot(timeline(:,3:end),OutputTIPA);
+xlabel('Time(s)')
+ylabel('PositionAcc(mm/s^2)')
+legend('X','Y','Z')
+title('Robot Tip Acceleration Vs Time')
+
 % Clear up memory upon termination
 pp.shutdown()
 clear java;
